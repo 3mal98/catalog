da86316c581f40c303e5da8b0962dff9fa9e4790
3mal98 <amalahmed_3@hotmail.com>
2022-09-23 12:14:48 +0100

    first commit

---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,18 @@
+// This is an asynchronous interface
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
